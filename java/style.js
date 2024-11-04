e87b7e1437d62f7c132c57068f7b9a5a8e5ad34b
Abderrezzak-Hamada <9024905@student.zadkine.nl>
2024-11-04 15:34:16 +0100

    java script calculator

--- java/style.js
+++
@@ -1,4 +0,0 @@
-function change() {document.getElementById('title').innerText = "Hello"
-
-    document.getElementById('para').style.color = 'blue'}
-                                                                                                                                                                                  
